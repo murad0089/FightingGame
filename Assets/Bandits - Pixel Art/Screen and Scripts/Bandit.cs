e40835bf93e30b72e7641085e665cd385015499e
Murad <murad.aliyev.13.murad@gmail.com>
2022-12-29 19:51:17 +0400

    First Commit

---
+++ Assets/Bandits - Pixel Art/Screen and Scripts/Bandit.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bandit : MonoBehaviour {
+
+    [SerializeField] float      m_speed = 4.0f;
+    [SerializeField] float      m_jumpForce = 7.5f;
+
+    private Animator            m_animator;
+    public Transform            attackPoint;
+    public float                attackRange = 0.5f;
+    public LayerMask            enemyLayer;
+    private Rigidbody2D         m_body2d;
+    private Sensor_Bandit       m_groundSensor;
+    private bool                m_grounded = false;
+    private bool                m_combatIdle = false;
+    private bool                m_isDead = false;
+
+    // Use this for initialization
+    void Start () {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Check if character just landed on the ground
+        if (!m_grounded && m_groundSensor.State()) {
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Check if character just started falling
+        if(m_grounded && !m_groundSensor.State()) {
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        // -- Handle input and movement --
+        float inputX = Input.GetAxis("Horizontal");
+
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (inputX < 0)
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Move
+        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+
+        //Set AirSpeed in animator
+        m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
+
+        // -- Handle Animations --
+        //Death
+        if (Input.GetKeyDown("e")) {
+            if(!m_isDead)
+                m_animator.SetTrigger("Death");
+            else
+                m_animator.SetTrigger("Recover");
+
+            m_isDead = !m_isDead;
+        }
+            
+        //Hurt
+
+        //Attack
+        else if(Input.GetKeyDown("q")) {
+            Attack();
+
+        }
+
+        //Change between idle and combat idle
+        else if (Input.GetKeyDown("f"))
+            m_combatIdle = !m_combatIdle;
+
+        //Jump
+        else if (Input.GetKeyDown("w") && m_grounded) {
+            m_animator.SetTrigger("Jump");
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            m_groundSensor.Disable(0.2f);
+        }
+
+        //Run
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+            m_animator.SetInteger("AnimState", 2);
+
+        //Combat Idle
+        else if (m_combatIdle)
+            m_animator.SetInteger("AnimState", 1);
+
+        //Idle
+        else
+            m_animator.SetInteger("AnimState", 0);
+    }
+    void Attack()
+    {
+        m_animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies= Physics2D.OverlapCircleAll(attackPoint.position,attackRange,enemyLayer);
+        foreach (Collider2D enemy in hitEnemies) {
+            enemy.GetComponent<Sensor_Bandit>().TakeDamage(20);
+            Debug.Log("We hit" + enemy.name);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ Assets/Bandits - Pixel Art/Screen and Scripts/Sensor_Bandit.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Sensor_Bandit : MonoBehaviour {
+
+    private Animator animator;
+    private int m_ColCount = 0;
+    public int maxHealth = 100;
+    int currentHealth;
+    private float m_DisableTimer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int Damage)
+    {
+        currentHealth -= Damage;
+
+        if(currentHealth < 0)
+        {
+            animator.SetTrigger("Death");
+            Debug.Log("enemy died");
+        }
+    }
+    private void OnEnable()
+    {
+        m_ColCount = 0;
+    }
+
+    public bool State()
+    {
+        if (m_DisableTimer > 0)
+            return false;
+        return m_ColCount > 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        m_ColCount++;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        m_ColCount--;
+    }
+
+    void Update()
+    {
+        m_DisableTimer -= Time.deltaTime;
+    }
+
+    public void Disable(float duration)
+    {
+        m_DisableTimer = duration;
+    }
+
+}
